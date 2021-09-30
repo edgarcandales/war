1090b0bb5aad96be72b358a24279ca00c6a77efe
Edgar Candales <68811563+edgarcandales@users.noreply.github.com>
2021-09-30 10:56:45 -0400

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Edgar Candales on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
